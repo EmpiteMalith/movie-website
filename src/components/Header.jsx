64299bcb8e3be206f8e7b836bd79de16e80cee50
empitemalith <malith@empite.com>
2023-01-14 10:50:02 +0530

    Navigation changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <div className="movieapp-header">
         <div className="logo">
-         <a href="/"><h3>Movie<span className="text-white">vik</span></h3> </a>
+         <Link to="/"><h3>Movie<span className="text-white">vik</span></h3></Link>
         </div>
 
     </div>
