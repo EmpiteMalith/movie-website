e079c04e1cffe8ffe3ef4d38170af3a51c723183
empitemalith <malith@empite.com>
2023-01-14 10:26:00 +0530

    Project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="movieapp-header">
+        <div className="logo">
+         <a href="/"><h3>Movie<span className="text-white">vik</span></h3> </a>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
